4f6a0f44bac9ac49e44212de3b11b5e9d9759187
hamidwise1 <hrawebdev@gmail.com>
2024-09-15 11:13:42 +0330

    add user struct and user package

--- main.go
+++ main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"example.com/user-input/note"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
+
+func getNoteData() (string, string) {
+	title := getUesrInput("note title:")
+	content := getUesrInput("note content:")
+
+	return title, content
+}
+
+func getUesrInput(prompt string) string {
+	fmt.Println(prompt)
+	var value string
+	fmt.Scanln(&value)
+
+	return value
+}
